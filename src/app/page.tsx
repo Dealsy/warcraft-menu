e825902642c4d9d489b4f7b943a4aa4d632bbbba
Matt Deal <contact@mattdeal.com.au>
2025-04-08 11:04:19 +1000

    [update]: got v0 prototype updated to new tailwind v4 version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-"use client"
+"use client";
 
-import { MenuLayout } from "@/components/menu-layout"
+import { MenuLayout } from "@/components/menu-layout";
 
 export default function HomePage() {
-  return <MenuLayout>{/* Home page content can be empty as we're showing the main menu */}</MenuLayout>
+  return (
+    <MenuLayout>
+      {/* Home page content can be empty as we're showing the main menu */}
+    </MenuLayout>
+  );
 }
